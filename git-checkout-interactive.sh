81891b5aa65deab5a75f26a7d2d927dafa23aafa
SimGus <simon.gustin@hotmail.com>
2020-07-26 12:48:13 +0200

    Refactor function order

--- git-checkout-interactive.sh
+++ git-checkout-interactive.sh
@@ -2,6 +2,49 @@
 
 grep_command="rg"
 
+__gchk_usage() {
+    echo "Git Checkout++ -- SimGus 2020"
+    echo "Usage: gchk [-i|--interactive] [<PARTIAL-BRANCH-NAME>]"
+    echo "\t<PARTIAL-BRANCH-NAME>\t\tThe name of a git branch (can be partial)"
+    echo "\t-i, --interactive\t\tRun the command in interactive mode"
+    echo "\t-r, --include-remote-branches\tIf the command runs interactively, only takes into account the remote branches"
+    echo "\t-a, --all\t\t\tIf the command runs interactively, take into account both the local and remote branches"
+    echo "\nFor information about Git's official checkout, please read 'man git checkout'."
+}
+gchk() {
+    if [ "$#" -eq 0 ]
+    then
+        echo "Too few arguments"
+        __gchk_usage
+        return 1
+    else
+        interactive=false
+        for arg in "$@"
+        do
+            shift
+            if [ "$arg" = "-i" ] || [ "$arg" = "--interactive" ]
+            then
+                interactive=true
+                continue
+            fi
+            set -- "$@" "$arg"
+        done
+        if [ "$interactive" = false ]
+        then
+            git checkout $@
+        else
+            git_checkout_interactive $@
+        fi
+    fi
+}
+
+__gci_usage() {
+    echo "Git Checkout Interactive -- SimGus 2020"
+    echo "Usage: git-checkout-interactive [-r|-a] [<PARTIAL-BRANCH-NAME>]"
+    echo "\t<PARTIAL-BRANCH-NAME>\t\tA pattern to look for in the branches names (filters out other branches)"
+    echo "\t-r, --include-remote-branches\t\tIf the command runs interactively, only takes into account the remote branches"
+    echo "\t-a, --all\t\t\tIf the command runs interactively, take into account both the local and remote branches"
+}
 git_checkout_interactive() {
     echo "Called git checkout interactive with args $@"
     if ! command -v fzf &> /dev/null
@@ -68,49 +111,4 @@ git_checkout_interactive() {
             git checkout "$selected_branch"
         fi
     fi
-}
-
-gchk() {
-    if [ "$#" -eq 0 ]
-    then
-        echo "Too few arguments"
-        __gchk_usage
-        return 1
-    else
-        interactive=false
-        for arg in "$@"
-        do
-            shift
-            if [ "$arg" = "-i" ] || [ "$arg" = "--interactive" ]
-            then
-                interactive=true
-                continue
-            fi
-            set -- "$@" "$arg"
-        done
-        if [ "$interactive" = false ]
-        then
-            git checkout $@
-        else
-            git_checkout_interactive $@
-        fi
-    fi
-}
-
-__gchk_usage() {
-    echo "Git Checkout++ -- SimGus 2020"
-    echo "Usage: gchk [-i|--interactive] [<PARTIAL-BRANCH-NAME>]"
-    echo "\t<PARTIAL-BRANCH-NAME>\t\tThe name of a git branch (can be partial)"
-    echo "\t-i, --interactive\t\tRun the command in interactive mode"
-    echo "\t-r, --include-remote-branches\tIf the command runs interactively, only takes into account the remote branches"
-    echo "\t-a, --all\t\t\tIf the command runs interactively, take into account both the local and remote branches"
-    echo "\nFor information about Git's official checkout, please read 'man git checkout'."
-}
-
-__gci_usage() {
-    echo "Git Checkout Interactive -- SimGus 2020"
-    echo "Usage: git-checkout-interactive [-r|-a] [<PARTIAL-BRANCH-NAME>]"
-    echo "\t<PARTIAL-BRANCH-NAME>\t\tA pattern to look for in the branches names (filters out other branches)"
-    echo "\t-r, --include-remote-branches\t\tIf the command runs interactively, only takes into account the remote branches"
-    echo "\t-a, --all\t\t\tIf the command runs interactively, take into account both the local and remote branches"
 }
