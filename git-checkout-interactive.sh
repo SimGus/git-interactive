97e60414fa40295e410ec1013fd743820d96d061
SimGus <simon.gustin@hotmail.com>
2020-07-26 12:31:38 +0200

    Add usage for both functions

--- git-checkout-interactive.sh
+++ git-checkout-interactive.sh
@@ -74,7 +74,7 @@ gchk() {
     if [ "$#" -eq 0 ]
     then
         echo "Too few arguments"
-        __gci_usage
+        __gchk_usage
         return 1
     else
         interactive=false
@@ -97,9 +97,20 @@ gchk() {
     fi
 }
 
+__gchk_usage() {
+    echo "Git Checkout++ -- SimGus 2020"
+    echo "Usage: gchk [-i|--interactive] [<PARTIAL-BRANCH-NAME>]"
+    echo "\t<PARTIAL-BRANCH-NAME>\t\tThe name of a git branch (can be partial)"
+    echo "\t-i, --interactive\t\tRun the command in interactive mode"
+    echo "\t-r, --include-remote-branches\tIf the command runs interactively, only takes into account the remote branches"
+    echo "\t-a, --all\t\t\tIf the command runs interactively, take into account both the local and remote branches"
+    echo "\nFor information about Git's official checkout, please read 'man git checkout'."
+}
+
 __gci_usage() {
     echo "Git Checkout Interactive -- SimGus 2020"
-    echo "Usage: gc [-i|--interactive] [<BRANCH-NAME>]"
-    echo "\t<BRANCH-NAME>\tThe name of a git branch (can be partial)"
-    echo "\t-i, --interactive\tRun the script as an interactive command"
+    echo "Usage: git-checkout-interactive [-r|-a] [<PARTIAL-BRANCH-NAME>]"
+    echo "\t<PARTIAL-BRANCH-NAME>\t\tA pattern to look for in the branches names (filters out other branches)"
+    echo "\t-r, --include-remote-branches\t\tIf the command runs interactively, only takes into account the remote branches"
+    echo "\t-a, --all\t\t\tIf the command runs interactively, take into account both the local and remote branches"
 }
