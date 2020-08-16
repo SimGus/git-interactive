3cfa27d19e31353c68aaa7b9021c9e3c8ae49ec1
SimGus <simon.gustin@hotmail.com>
2020-08-17 00:20:08 +0200

    Make util functions for checkout script local

--- git-checkout-interactive.sh
+++ git-checkout-interactive.sh
@@ -47,6 +47,58 @@ __gci_usage() {
     echo "\t-a, --all\t\t\tIf the command runs interactively, take into account both the local and remote branches"
 }
 git_checkout_interactive() {
+    __gci_checkout() {
+        local include_remote_branches=false
+        for arg in "$@"
+        do
+            if [ "$arg" = "--include-remote-branches" ]
+            then
+                include_remote_branches=true
+                continue
+            else
+                local selected_branch="$arg"
+            fi
+        done
+
+        if [ "$include_remote_branches" = false ]
+        then
+            git checkout $selected_branch
+        else
+            local remote_name="$(echo $selected_branch | sed 's:remotes/::')"
+            local corresponding_local_branch="$(__gci_find_corresponding_local_branch $remote_name)"
+            if [ -z "$corresponding_local_branch" ]
+            then
+                git checkout -t $selected_branch
+            else
+                echo "Local branch '$corresponding_local_branch' already tracks '$remote_name'"
+                echo -n "Create a new local branch with another name? [y/n] "
+                local create_local_branch
+                read create_local_branch
+                if [[ $create_local_branch =~ [yY] ]]
+                then
+                    echo -n "Please specify the name of the new branch: "
+                    local new_branch_name
+                    read new_branch_name
+                    if [ -n "$new_branch_name" ]
+                    then
+                        git checkout -b $new_branch_name $remote_name
+                    else
+                        echo "No name provided"
+                        return 1
+                    fi
+                fi
+            fi
+        fi
+    }
+    __gci_find_corresponding_local_branch() {
+        if [ "$#" -ne 1 ]
+        then
+            return 0
+        else
+            echo "$(git branch -vv | eval $__gi_grep_command $remote_name | cut -d' ' -f2)"
+        fi
+    }
+
     if [ "$(__gi_is_git_repo)" = false ]
     then
         echo "Fatal: not in a get repository"
@@ -99,57 +151,5 @@ git_checkout_interactive() {
             fi
             __gci_checkout $include_remote_branches_flag $selected_branches
         fi
-    fi
-}
-
-__gci_checkout() {
-    local include_remote_branches=false
-    for arg in "$@"
-    do
-        if [ "$arg" = "--include-remote-branches" ]
-        then
-            include_remote_branches=true
-            continue
-        else
-            local selected_branch="$arg"
-        fi
-    done
-
-    if [ "$include_remote_branches" = false ]
-    then
-        git checkout $selected_branch
-    else
-        local remote_name="$(echo $selected_branch | sed 's:remotes/::')"
-        local corresponding_local_branch="$(__gci_find_corresponding_local_branch $remote_name)"
-        if [ -z "$corresponding_local_branch" ]
-        then
-            git checkout -t $selected_branch
-        else
-            echo "Local branch '$corresponding_local_branch' already tracks '$remote_name'"
-            echo -n "Create a new local branch with another name? [y/n] "
-            local create_local_branch
-            read create_local_branch
-            if [[ $create_local_branch =~ [yY] ]]
-            then
-                echo -n "Please specify the name of the new branch: "
-                local new_branch_name
-                read new_branch_name
-                if [ -n "$new_branch_name" ]
-                then
-                    git checkout -b $new_branch_name $remote_name
-                else
-                    echo "No name provided"
-                    return 1
-                fi
-            fi
-        fi
-    fi
-}
-__gci_find_corresponding_local_branch() {
-    if [ "$#" -ne 1 ]
-    then
-        return 0
-    else
-        echo "$(git branch -vv | eval $__gi_grep_command $remote_name | cut -d' ' -f2)"
     fi
 }
